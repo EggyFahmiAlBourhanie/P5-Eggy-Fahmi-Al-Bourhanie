ef7281ff7d1a0e0f085c0786277cd20cb0c47215
EggyFahmiAlBourhanie <64634533+EggyFahmiAlBourhanie@users.noreply.github.com>
2020-05-14 12:24:20 +0800

    Create jobsheet 1 C++.cpp
    Deference (&) operator yang berfungsi untuk menanyakan alamat dari suatu variabel

---
+++ jobsheet 1 C++.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main(){
+	int a=1;
+	float b=1.2;
+	double c=4;
+	char d='a';
+	bool e=1;
+	
+	cout << "alamat variabel a= "<<&a<<endl;
+	cout << "alamat variabel b= "<<&b<<endl;
+	cout << "alamat variabel a= "<<&c<<endl;
+	cout << "alamat variabel a= "<<&d<<endl;
+	cout << "alamat variabel a= "<<&e<<endl;
+	
+	cout << "\nNilai alamat variabel a= "<<a<<endl;
+	cout << "Nilai alamat variabel b= "<<b<<endl;
+	cout << "Nilai alamat variabel a= "<<c<<endl;
+	cout << "Nilai alamat variabel a= "<<d<<endl;
+	cout << "Nilai alamat variabel a= "<<e<<endl;
+}
