ea96c0be3ee97fbb57176eae9a95dcf549e6c962
EggyFahmiAlBourhanie <64634533+EggyFahmiAlBourhanie@users.noreply.github.com>
2020-05-14 12:27:10 +0800

    Create jobsheet 4 C++.cpp
    Penggunaan pointer pada data string statis

---
+++ jobsheet 4 C++.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+using namespace std;
+
+int main(void){ 
+
+ char*bio = "Nama \t\t: Eggy fahmi al bourhanie\n\nTgl lahir \t: Taliwang, 01 Agustus 2000\n\nalamat \t\t: Karang Dima, Sumbawa Besar\n\nuniversitas \t: universitas mataram\n\n";
+ cout<<bio;
+ return 0;
+}
